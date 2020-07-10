187f65cb0262ba997991368c19c10472b9a34643
Raffl Christopher <chra@ig.loc>
2020-07-10 14:30:30 +0200

    Getting closer

--- mmmisp/webui/extension.js
+++ mmmisp/webui/extension.js
@@ -1,5 +1,24 @@
 console.log('Loading mmmisp WebUI');
-console.log(window.location.pathname);
+var _getAllFilesFromFolder = function(dir) {
+
+    var filesystem = require("fs");
+    var results = [];
+
+    filesystem.readdirSync(dir).forEach(function(file) {
+
+        file = dir+'/'+file;
+        var stat = filesystem.statSync(file);
+
+        if (stat && stat.isDirectory()) {
+            results = results.concat(_getAllFilesFromFolder(file))
+        } else results.push(file);
+
+    });
+
+    return results;
+
+};
+console.log(_getAllFilesFromFolder(__dirname + "folder"));
 
 (function() {
 
